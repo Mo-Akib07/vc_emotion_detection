a817aad95aad85c04b60beaee148459e721ae7d3
mo-akib-07 <akibkhan01561@outlook.com>
2025-06-29 18:48:40 +0530

    add cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='a DVC Version control project',
+    author='Mohammad Akib',
+    license='MIT',
+)
